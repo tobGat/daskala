565ec69c6ec2adc286a8dbb828f3351d3966fddd
Tobias Gatterbauer <tobiasgatterbauer@gmail.com>
2026-07-31 11:27:42 +0200

    refactor(core): todos herausgeloest (rein DB)

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ const einstellungenDomain = require('./core/domain/einstellungen')
 const schuljahreDomain = require('./core/domain/schuljahre')
 const notizenDomain = require('./core/domain/notizen')
 const termineDomain = require('./core/domain/termine')
+const todosDomain = require('./core/domain/todos')
 // main.js-Helfer, die extrahierte Domänen injiziert bekommen. pushUndo und
 // berechneAlleFuerSchuljahr sind hoisted function-Deklarationen weiter unten,
 // stehen hier also bereits zur Verfügung. Wächst mit der Extraktion.
@@ -2848,43 +2849,11 @@ function registerIPC() {
   })
 
   // Todos
-  ipcMain.handle('todos:getAll', (_, schuljahrId) => {
-    return db.prepare(`
-      SELECT t.*, k.name as klasse_name, f.name as fach_name
-      FROM todos t
-      LEFT JOIN klassen k ON k.id = t.klasse_id
-      LEFT JOIN faecher f ON f.id = t.fach_id
-      WHERE t.klasse_id IS NULL OR k.schuljahr_id = ?
-      ORDER BY t.erledigt, t.reihenfolge, t.id
-    `).all(schuljahrId)
-  })
-
-  ipcMain.handle('todos:create', (_, { titel, klasseId, fachId, faelligkeit, erinnerung }) => {
-    console.log('[main] todos:create:', { titel, faelligkeit, erinnerung })
-    const maxReihenfolge = klasseId
-      ? db.prepare('SELECT MAX(reihenfolge) as m FROM todos WHERE klasse_id = ?').get(klasseId)?.m ?? 0
-      : db.prepare('SELECT MAX(reihenfolge) as m FROM todos WHERE klasse_id IS NULL').get()?.m ?? 0
-    const info = db.prepare(
-      'INSERT INTO todos (titel, klasse_id, fach_id, faelligkeit, erinnerung, reihenfolge) VALUES (?, ?, ?, ?, ?, ?)'
-    ).run(titel, klasseId ?? null, fachId ?? null, faelligkeit ?? null, erinnerung ?? null, maxReihenfolge + 1)
-    return info.lastInsertRowid
-  })
-
-  ipcMain.handle('todos:update', (_, id, { titel, klasseId, fachId, faelligkeit, erinnerung }) => {
-    db.prepare('UPDATE todos SET titel = ?, klasse_id = ?, fach_id = ?, faelligkeit = ?, erinnerung = ? WHERE id = ?')
-      .run(titel, klasseId ?? null, fachId ?? null, faelligkeit ?? null, erinnerung ?? null, id)
-    return true
-  })
-
-  ipcMain.handle('todos:delete', (_, id) => {
-    db.prepare('DELETE FROM todos WHERE id = ?').run(id)
-    return true
-  })
-
-  ipcMain.handle('todos:toggleErledigt', (_, id) => {
-    db.prepare('UPDATE todos SET erledigt = CASE WHEN erledigt = 0 THEN 1 ELSE 0 END WHERE id = ?').run(id)
-    return true
-  })
+  ipcMain.handle('todos:getAll', (_, schuljahrId) => todosDomain.getAll(db, schuljahrId))
+  ipcMain.handle('todos:create', (_, data) => todosDomain.create(db, data))
+  ipcMain.handle('todos:update', (_, id, data) => todosDomain.update(db, id, data))
+  ipcMain.handle('todos:delete', (_, id) => todosDomain.remove(db, id))
+  ipcMain.handle('todos:toggleErledigt', (_, id) => todosDomain.toggleErledigt(db, id))
 
   // Backup
   ipcMain.handle('backup:create', () => doBackupCreate())
